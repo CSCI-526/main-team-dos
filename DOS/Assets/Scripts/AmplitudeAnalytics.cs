8f4d79326421d6fa6fcf62419eb778f06851d6f4
anirudh-rv <anirudh.rv1234@gmail.com>
2025-10-11 18:49:14 -0700

    Analytics fixes

--- DOS/Assets/Scripts/AmplitudeAnalytics.cs
+++ DOS/Assets/Scripts/AmplitudeAnalytics.cs
@@ -3,11 +3,26 @@ using UnityEngine.Networking;
 using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System;
 
 public class AmplitudeAnalytics : MonoBehaviour
 {
     private const string API_KEY = "5e89e99bb579654bfbc0cd077b04c8e9";
     private const string ENDPOINT = "https://api2.amplitude.com/2/httpapi";
+    private const string DEVICE_ID_KEY = "amplitude_device_id";
+    
+    private static string GetOrCreateDeviceId()
+    {
+        string deviceId = PlayerPrefs.GetString(DEVICE_ID_KEY, "");
+        if (string.IsNullOrEmpty(deviceId))
+        {
+            deviceId = Guid.NewGuid().ToString();
+            PlayerPrefs.SetString(DEVICE_ID_KEY, deviceId);
+            PlayerPrefs.Save();
+        }
+        return deviceId;
+    }
+
 
     public void LogEvent(string eventName, Dictionary<string, object> properties = null)
     {
@@ -16,8 +31,11 @@ public class AmplitudeAnalytics : MonoBehaviour
 
     private IEnumerator SendEvent(string eventName, Dictionary<string, object> properties)
     {
+        string deviceId = GetOrCreateDeviceId();
         var eventObj = new Dictionary<string, object>
         {
+            { "user_id", "webuser" },
+            { "device_id", deviceId },
             { "event_type", eventName },
             { "event_properties", properties ?? new Dictionary<string, object>() }
         };
@@ -41,7 +59,11 @@ public class AmplitudeAnalytics : MonoBehaviour
             yield return www.SendWebRequest();
 
             if (www.result != UnityWebRequest.Result.Success)
+            {
                 Debug.LogError($"Amplitude HTTP Error: {www.error}");
+                Debug.LogError($"response body: {www.downloadHandler.text}");
+            }
+
             else
                 Debug.Log($"Amplitude HTTP Event Sent: {eventName} with properties: {JsonConvert.SerializeObject(properties)}");
         }
