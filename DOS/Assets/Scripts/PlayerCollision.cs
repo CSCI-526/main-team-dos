b838a43453c17f838dbdba3370a2e1b6e26c2119
Yeefong Wu <79526658+yeefongwu@users.noreply.github.com>
2025-10-26 13:15:52 -0700

    DOS-49 (#27)

--- DOS/Assets/Scripts/PlayerCollision.cs
+++ DOS/Assets/Scripts/PlayerCollision.cs
@@ -5,6 +5,8 @@ using TMPro;
 public class PlayerCollision : MonoBehaviour
 {
     public TextMeshProUGUI missionFailedText;
+    [SerializeField] private GameObject missionFailedPanel;
+
     private bool isFailing = false;
     
     
@@ -28,19 +30,15 @@ public class PlayerCollision : MonoBehaviour
 
             // Show the "MISSION FAILED" text
             missionFailedText.text = "MISSION FAILED";
+            missionFailedPanel.SetActive(true);
+
 
             // Freeze the game
             Time.timeScale = 0f;
 
-            // Start the coroutine to restart the scene
-            StartCoroutine(RestartScene());
+        
         }
     }
 
-    private System.Collections.IEnumerator RestartScene()
-    {
-        yield return new WaitForSecondsRealtime(2f);
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("Starter");
-    }
+    
 }
