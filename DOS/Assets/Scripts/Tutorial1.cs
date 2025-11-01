02a118d322d622e4c4c8b4588860c55d6a200dbb
anirudh-rv <anirudh.rv1234@gmail.com>
2025-10-31 17:41:19 -0700

    Tutorial changes

--- DOS/Assets/Scripts/Tutorial1.cs
+++ DOS/Assets/Scripts/Tutorial1.cs
@@ -12,7 +12,7 @@ public class Tutorial1 : MonoBehaviour
         "Press 'W/↑' To Jump",
         "Shoot Blue Portal Using Left Click\nYou can only Shoot Portals on the Brown Surfaces",
         "Shoot Orange Portal Using Right Click\nYou can only Shoot Portals on the Brown Surfaces",
-        "Jump Into The Portals",
+        "Jump Into The Portals\nYou can CLEAR the portals by pressing R",
     };
 
     private int instructionIndex = 0;
